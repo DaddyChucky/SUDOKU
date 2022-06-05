c4cf98302c389d9ed71d1e6131f82d1d80edf88c
Charles De Lafontaine <46151389+DaddyChucky@users.noreply.github.com>
2022-06-05 00:51:20 -0400

    generate_game & generate_slots_and_helpers funcs

--- src/main.py
+++ src/main.py
@@ -9,27 +9,37 @@ import static.constants as globals
 
 class Sudoku:
     """
-    A class used to represent a Sudoku
-
-    ...
+    Class for Sudoku!
 
     Attributes
     ----------
-    attribute1 : TypeHere
-    Description goes here
+    logger: Logger
+        The main file logger of Sudoku.
+    slots: list
+        Slots of the Sudoku (count=81 in a normal Sudoku game).
+    helpers: list
+        Helpers of the Sudoku (maximum 81 times 9 at the starting point).
 
     Methods
     -------
-    method(param1=TypeHere)
-    Description goes here
+    __init__(logging_file_name: str, logging_level: Function)
+    play()
     """
 
     def __init__(self, logging_file_name: str, logging_level) -> None:
-        """
+        """ Initializes the Sudoku class.
+
         Parameters
         ----------
-        param1 : type
-            Desc
+        logging_file_name : str
+            File name of the logging file, including its type (preferably .log).
+        logging_level : Function
+            Logging level to be displayed in the logging file (please refer to the Logging module documentation).
+        
+        Raises
+        ------
+        Exception
+            Any exception caused by the Logging module upon initialization of the Logger.
         """
         try:
             logging.basicConfig(filename=logging_file_name, format=globals.DEFAULT_LOGGING_FORMAT, filemode=globals.DEFAULT_LOGGING_FILE_MODE)
@@ -37,23 +47,63 @@ class Sudoku:
             self.logger.setLevel(logging_level)
             self.logger.debug('Entering Sudoku::__init__')
             self.logger.info('Logger successfully instantiated')
+            self.logger.info('Initializing attributes')
+            self.slots          = []
+            self.helpers        = []
+            self.helpers_sizes  = []
+            self.logger.info('Done initializing attributes')
+            self.logger.info('Attributes info:')
+            self.logger.info('Slots:')
+            self.logger.info(self.slots)
+            self.logger.info('Helpers:')
+            self.logger.info(self.helpers)
+            self.logger.info('Size of helpers:')
+            self.logger.info(self.helpers_sizes)
             self.logger.debug('Leaving Sudoku::__init__')
         except Exception as e:
             self.logger.exception(e)
 
     def play(self):
-        """Application starting point by launching a fresh Sudoku game.
+        """Launches a fresh Sudoku game.
 
         Raises
         ------
         Exception
-            Exception clause.
+            Any exception non catched by function calls.
         """
-        self.logger.debug('Entering Sudoku::play')
+        try:
+            self.logger.debug('Entering Sudoku::play')
+            self.generate_game()
+            self.logger.debug('Leaving Sudoku::play')
+        except Exception as e:
+            self.logger.exception(e)
+    
+    def generate_game(self):
+        self.logger.debug('Entering Sudoku::generate_game')
 
+        self.logger.info('Generating game matrixes')
+        self.generate_slots_and_helpers()
 
+        self.logger.debug('Leaving Sudoku::generate_game')
+    
+    def generate_slots_and_helpers(self):
+        self.logger.debug('Entering Sudoku::generate_slots')
 
-        self.logger.debug('Leaving Sudoku::play')
+        self.logger.info('Generating slots and helpers')
+        for _ in range(globals.N_SLOTS):
+            self.slots.append(globals.NO_ENTRY)
+            self.helpers.append(globals.DEFAULT_HELPERS)
+            self.helpers_sizes.append(len(globals.DEFAULT_HELPERS))
+        self.logger.info('Done generating slots and helpers')
+        self.logger.info('Attributes info:')
+        self.logger.info('Slots:')
+        self.logger.info(self.slots)
+        self.logger.info('Helpers:')
+        self.logger.info(self.helpers)
+        self.logger.info('Size of helpers:')
+        self.logger.info(self.helpers_sizes)
+        self.logger.debug('Leaving Sudoku::generate_slots')
+
 
 sudoku: Sudoku = Sudoku('logging.log', logging.DEBUG)
 sudoku.play()
--- src/static/constants.py
+++ src/static/constants.py
@@ -1,2 +1,11 @@
-DEFAULT_LOGGING_FORMAT:     str = '%(asctime)s %(message)s'
-DEFAULT_LOGGING_FILE_MODE:  str = 'w'
+import logging
+
+DEFAULT_LOGGING_FORMAT:     str     = '%(asctime)s %(message)s'
+DEFAULT_LOGGING_FILE_MODE:  str     = 'w'
+DEFAULT_LOGGING_FILE_NAME:  str     = 'logging.log'
+DEFAULT_LOGGING_LEVEL               = logging.DEBUG
+N_COLUMNS:                  int     = 9
+N_LINES:                    int     = N_COLUMNS
+N_SLOTS:                    int     = N_COLUMNS * N_LINES
+DEFAULT_HELPERS:            list    = [1, 2, 3, 4, 5, 6, 7, 8, 9] 
+NO_ENTRY:                   int     = 0
