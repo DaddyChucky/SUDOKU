aafd332bfe340df173c5a96145e5cbca60a48aae
Charles <46151389+DaddyChucky@users.noreply.github.com>
2022-06-16 01:37:06 -0400

    PEP8 friendly

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
-__name__    = "SUDOKU"
+__name__ = "SUDOKU"
 __version__ = "1.0"
-__author__  = "Charles De Lafontaine (DaddyChucky)"
+__author__ = "Charles De Lafontaine (DaddyChucky)"
 __license__ = "MIT"
 
 import logging
-import static.constants as globals
+import static.constants as globs
 from random import randint
 from copy import deepcopy
 
@@ -46,15 +46,16 @@ class Sudoku:
             Any exception caused by the Logging module upon initialization of the Logger.
         """
         try:
-            logging.basicConfig(filename=logging_file_name, format=globals.DEFAULT_LOGGING_FORMAT, filemode=globals.DEFAULT_LOGGING_FILE_MODE)
+            logging.basicConfig(filename=logging_file_name, format=globs.DEFAULT_LOGGING_FORMAT,
+                                filemode=globs.DEFAULT_LOGGING_FILE_MODE)
             self.logger = logging.getLogger()
             self.logger.setLevel(logging_level)
             self.logger.debug('Entering Sudoku::__init__')
             self.logger.info('Logger successfully instantiated')
             self.logger.info('Initializing attributes')
-            self.slots          = []
-            self.helpers        = []
-            self.helpers_sizes  = []
+            self.slots = []
+            self.helpers = []
+            self.helpers_sizes = []
             self.filled_indexes = []
             self.logger.info('Done initializing attributes')
             self.logger.info('Attributes info:')
@@ -82,19 +83,19 @@ class Sudoku:
             self.logger.debug('Leaving Sudoku::play')
         except Exception as e:
             self.logger.exception(e)
-    
+
     def generate_game(self) -> None:
         self.logger.debug('Entering Sudoku::generate_game')
         self.logger.info('Generating game matrixes')
         self.generate_slots_and_helpers()
         self.logger.debug('Leaving Sudoku::generate_game')
-    
+
     def generate_slots_and_helpers(self) -> None:
         self.logger.debug('Entering Sudoku::generate_slots')
         self.logger.info('Generating slots and helpers')
-        for _ in range(globals.N_SLOTS):
-            self.slots.append(globals.NO_ENTRY)
-            default_helpers_dc: list = deepcopy(globals.DEFAULT_HELPERS)
+        for _ in range(globs.N_SLOTS):
+            self.slots.append(globs.NO_ENTRY)
+            default_helpers_dc: list = deepcopy(globs.DEFAULT_HELPERS)
             self.helpers.append(default_helpers_dc)
             self.helpers_sizes.append(len(default_helpers_dc))
         self.logger.info('Done generating slots and helpers')
@@ -106,17 +107,20 @@ class Sudoku:
         self.logger.debug('Size of helpers:')
         self.logger.debug(self.helpers_sizes)
         self.logger.debug('Leaving Sudoku::generate_slots')
-    
+
     def choose_random_slot(self) -> tuple:
         self.logger.debug('Entering Sudoku::choose_random_slot')
 
         self.logger.info('Choosing a random slot')
-        helpers_min_indexes:    list    = []
-        helpers_min_val:        int     = min(self.helpers_sizes)
+        helpers_min_indexes: list = []
+        helpers_min_val: int = min(self.helpers_sizes)
         for i, helpers_size in enumerate(deepcopy(self.helpers_sizes)):
-            if (helpers_size == helpers_min_val or helpers_size > globals.MIN_HELPERS_SIZE_BYPASS) and helpers_size != 0 and i not in self.filled_indexes:
+            if (
+                    helpers_size == helpers_min_val or helpers_size > globs.MIN_HELPERS_SIZE_BYPASS) \
+                    and helpers_size != 0 and i not in self.filled_indexes:
                 helpers_min_indexes.append(i)
         if len(helpers_min_indexes) - 1 == 0:
+            print('workign')
             self.generate_end_game()
             exit(-1)
         self.logger.info('Done choosing a random slot')
@@ -127,11 +131,11 @@ class Sudoku:
         self.logger.info('Choosing an index')
         print(len(helpers_min_indexes) - 1)
         print(helpers_min_indexes)
-        chosen_helpers_min_index:               int     = randint(0, len(helpers_min_indexes) - 1)
-        chosen_index:                           int     = helpers_min_indexes[chosen_helpers_min_index]
-        helpers_at_chosen_slot_index:           list    = self.helpers[chosen_index]
-        index_of_helper_at_chosen_slot_index:   int     = randint(0, len(helpers_at_chosen_slot_index) - 1)
-        value_of_helper_at_chosen_slot_index:   int     = helpers_at_chosen_slot_index[index_of_helper_at_chosen_slot_index]
+        chosen_helpers_min_index: int = randint(0, len(helpers_min_indexes) - 1)
+        chosen_index: int = helpers_min_indexes[chosen_helpers_min_index]
+        helpers_at_chosen_slot_index: list = self.helpers[chosen_index]
+        index_of_helper_at_chosen_slot_index: int = randint(0, len(helpers_at_chosen_slot_index) - 1)
+        value_of_helper_at_chosen_slot_index: int = helpers_at_chosen_slot_index[index_of_helper_at_chosen_slot_index]
         self.logger.info('Done choosing an index')
         self.logger.info('Chosen helpers min index:')
         self.logger.info(chosen_helpers_min_index)
@@ -155,13 +159,13 @@ class Sudoku:
         self.logger.debug(self.filled_indexes)
         self.logger.info('Done updating filled indexes')
         self.logger.info('Updating row helpers')
-        row_index: int = chosen_slot_index // globals.N_ROWS
+        row_index: int = chosen_slot_index // globs.N_ROWS
         self.logger.debug('Row index:')
         self.logger.debug(row_index)
-        min_row_range: int = row_index * (globals.N_ROWS)
+        min_row_range: int = row_index * globs.N_ROWS
         self.logger.debug('Min row range:')
         self.logger.debug(min_row_range)
-        max_row_range: int = (row_index + 1) * (globals.N_ROWS) - 1
+        max_row_range: int = (row_index + 1) * globs.N_ROWS - 1
         self.logger.debug('Max row range:')
         self.logger.debug(max_row_range)
         for i in range(min_row_range, max_row_range + 1):
@@ -179,14 +183,14 @@ class Sudoku:
         self.logger.info('Done updating column helpers')
 
         self.logger.debug('Leaving Sudoku::update_helpers')
-    
+
     def generate_end_game(self):
         self.logger.debug('Entering Sudoku::generate_end_game')
         self.logger.info('Player reached the end of the game')
         self.logger.debug('Leaving Sudoku::generate_end_game')
 
+
 sudoku: Sudoku = Sudoku('logging.log', logging.DEBUG)
 sudoku.play()
-for i in range(80):
+for _ in range(80):
     sudoku.choose_random_slot()
-
--- src/static/constants.py
+++ src/static/constants.py
@@ -1,34 +1,34 @@
 import logging
 
-DEFAULT_LOGGING_FORMAT:     str     = '%(asctime)s %(message)s'
-DEFAULT_LOGGING_FILE_MODE:  str     = 'w'
-DEFAULT_LOGGING_FILE_NAME:  str     = 'logging.log'
-DEFAULT_LOGGING_LEVEL               = logging.DEBUG
-N_COLUMNS:                  int     = 9
-N_ROWS:                     int     = N_COLUMNS
-N_SLOTS:                    int     = N_COLUMNS * N_ROWS
-DEFAULT_HELPERS:            list    = [1, 2, 3, 4, 5, 6, 7, 8, 9] 
-NO_ENTRY:                   int     = 0
-MIN_HELPERS_SIZE_BYPASS:    int     = 1
-SQUARES_OFFSETS:            dict    = {
-    0:  [10,    11, 19, 20],
-    1:  [9,     11, 18, 20],
-    2:  [9,     10, 18, 19],
-    3:  [13,    14, 22, 23],
-    4:  [12,    14, 21, 23],
-    5:  [12,    13, 21, 22],
-    6:  [16,    17, 25, 26],
-    7:  [15,    17, 24, 26],
-    8:  [15,    16, 24, 25],
-    9:  [1,     2,  19, 20],
-    10: [0,     2,  18, 20],
-    11: [0,     1,  18, 19],
-    12: [4,     5,  22, 23],
-    13: [3,     5,  21, 23],
-    14: [3,     4,  21, 22],
-    15: [7,     8,  25, 26],
-    16: [6,     8,  24, 26],
-    17: [6,     7,  24, 25],
+DEFAULT_LOGGING_FORMAT: str = '%(asctime)s %(message)s'
+DEFAULT_LOGGING_FILE_MODE: str = 'w'
+DEFAULT_LOGGING_FILE_NAME: str = 'logging.log'
+DEFAULT_LOGGING_LEVEL = logging.DEBUG
+N_COLUMNS: int = 9
+N_ROWS: int = N_COLUMNS
+N_SLOTS: int = N_COLUMNS * N_ROWS
+DEFAULT_HELPERS: list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+NO_ENTRY: int = 0
+MIN_HELPERS_SIZE_BYPASS: int = 1
+SQUARES_OFFSETS: dict = {
+    0: [10, 11, 19, 20],
+    1: [9, 11, 18, 20],
+    2: [9, 10, 18, 19],
+    3: [13, 14, 22, 23],
+    4: [12, 14, 21, 23],
+    5: [12, 13, 21, 22],
+    6: [16, 17, 25, 26],
+    7: [15, 17, 24, 26],
+    8: [15, 16, 24, 25],
+    9: [1, 2, 19, 20],
+    10: [0, 2, 18, 20],
+    11: [0, 1, 18, 19],
+    12: [4, 5, 22, 23],
+    13: [3, 5, 21, 23],
+    14: [3, 4, 21, 22],
+    15: [7, 8, 25, 26],
+    16: [6, 8, 24, 26],
+    17: [6, 7, 24, 25],
     18: [1, 2, 10, 11],
     19: [0, 2, 9, 11],
     20: [0, 1, 9, 10],
